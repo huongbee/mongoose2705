4f431c6e39cea7432e9e69ef6c8ad1b41b1d3cac
Huong <huongnguyenak96@gmail.com>
2019-06-28 20:12:10 +0700

    mongoose

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/mongoose2705',{
+    useNewUrlParser: true
+})
+.then(()=>console.log('DB Connected!'))
+.catch((err)=>console.log(err.message))
